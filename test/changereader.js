8247f19e3a35ae48441f785fa0e87b48cf75337f
Glynn Bird <glynn.bird@gmail.com>
2018-05-14 11:53:49 +0100

    tests passing

--- test/changereader.js
+++ test/changereader.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* global describe it afterEach */
+/* global describe it before after afterEach */
 'use strict';
 
 const assert = require('assert');
